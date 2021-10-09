e070c0a872d3202ebb48c3e19a37e04864dd3523
Chris Pinnock <chris_pinnock@mac.com>
2021-10-09 08:51:27 +0000

    skip config

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Defaults
+# Defaults - override in the configuration file (shell)
 #
 logging=stdout
 background=0
+dontconfig=0
 
 if [ -z "$1" ]; then
 	echo "Usage: $0 configfile"
@@ -30,6 +31,11 @@ fi
 # Setup
 #
 if [ ! -d "$datadir" ] || [ ! -f "$datadir/config.json" ]; then
+
+	# Sometimes we will want to setup a node manually
+	#
+	[ "$dontconfig" = "1" ] && echo "Skipping configuration!" && exit 1
+
 	echo "Setting up configuration"
 	mkdir -p "$datadir"
 	$tezosnode config init 	--data-dir=$datadir \
