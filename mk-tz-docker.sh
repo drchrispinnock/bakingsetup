9d947b49c4a20460822988cc910c441e4fa74cfc
Chris Pinnock <chris_pinnock@mac.com>
2021-12-21 14:44:31 +0000

    merge

--- mk-tz-docker.sh
+++ mk-tz-docker.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# You need:
+#
+# 1. A copy of the Tezos software in $dir
+# 2. Suggest you opam init, make build-deps, etc
+# 3. Install docker and docker.io
+# 4. Run this on arm64 & x86 for the resultant dockers
+
 arch=`uname -m`
 date=`date +%Y%m%d`
 rootname="tezos-12rc1"
