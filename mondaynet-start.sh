00d949ff77f635b05fb681d7eb8fe121c2901994
Chris Pinnock <chris_pinnock@mac.com>
2022-01-11 17:42:08 +0000

    more monday net work

--- mondaynet-start.sh
+++ mondaynet-start.sh
@@ -11,7 +11,7 @@ startconf=$HOME/startup/mondaynet.conf
 # Config
 #
 if [ -f "$HOME/monday.setup" ]; then
-	source $HOME/monday.setup
+	. $HOME/monday.setup
 fi
 
 # Do not change these
@@ -33,20 +33,28 @@ if [ ! -f "$HOME/.firstrun" ]; then
 	sudo apt update
 	sudo apt install -y rsync git m4 build-essential patch unzip wget pkg-config libgmp-dev libev-dev libhidapi-dev libffi-dev opam jq zlib1g-dev bc autoconf
 
+	echo "==> Installing rust"
+	echo ""
 	wget https://sh.rustup.rs/rustup-init.sh
 	chmod +x rustup-init.sh
 	./rustup-init.sh --profile minimal --default-toolchain 1.52.1 -y
 	touch "$HOME/.firstrun"
 fi
-source $HOME/.cargo/env
+. $HOME/.cargo/env
+echo $PATH
+sleep 5
 
 # Update the software to latest master branch of Octez
 #
 if [ ! -d $builddir ]; then
 	mkdir -p "$buildroot"
+	cd $buildroot
 	git clone $gitrepos
-	opam --init bare
+	cd tezos
+	opam init --bare --yes
 fi
+
+eval $(opam env) 
 
 cd $builddir
 git checkout $branch
@@ -62,9 +70,17 @@ fi
 
 # Reset baking account
 #
+echo "===> Resetting baking accounts"
+echo ""
 
-
+sleep 30
 
 # Start the node
 #
+echo "===> Starting the nodes and baking daemons"
+echo ""
+
+# Start the node
+#
+$startscript $startconf
 $startscript $startconf
