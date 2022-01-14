66831a29c7a020c080f3544735b847ea754d1f3a
Chris Pinnock <chris_pinnock@mac.com>
2022-01-14 13:19:23 +0000

    delegate

--- mondaynet/delegate.sh
+++ mondaynet/delegate.sh
@@ -1,4 +1,15 @@
+key=`hostname -s`
 
-# Example
-../tezos/tezos-client activate account $key with mondaynet/$key/faucet.json
-../tezos/tezos-client register key $key as delegate
+if [ -f "faucet.json" ]; then
+	./tezos/tezos-client activate account $key with faucet.json
+	./tezos/tezos-client register key $key as delegate
+
+	if [ -f "startup/mondaynet/wallet-$key/public_keys" ]; then
+		echo "Warning - already keys in the repo"
+	else
+		cp ~/.tezos-client/*key* startup/mondaynet/wallet-$key
+		mv faucet.json startup/mondaynet/wallet-$key
+		cd startup/mondaynet/wallet-$key
+		git add .
+	fi
+fi
