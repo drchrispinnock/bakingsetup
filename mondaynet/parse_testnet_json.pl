6e8526847cbf1b83517dd0d6cc55d08f2e50a4bf
Chris Pinnock <chris_pinnock@mac.com>
2022-01-18 19:28:36 +0000

    match on network stub

--- mondaynet/parse_testnet_json.pl
+++ mondaynet/parse_testnet_json.pl
@@ -9,7 +9,8 @@ use Data::Dumper;
 die unless(@ARGV);
 
 my $file = $ARGV[0];
-my $network = $ARGV[1];
+my $stub = $ARGV[1];
+my $network = $stub;
 
 my $json_text ="";
 open FILE, "$ARGV[0]" or die "cannot open file\n";
@@ -21,8 +22,12 @@ close FILE;
 my $json = JSON->new->allow_nonref;
 my $hd = $json->decode($json_text);
 
-#print Dumper($hd);
-#print "\n";
+my %h = %$hd;
+#print "Available networks in json file:\n"; 
+foreach my $k (keys(%h)) {
+#	warn "\t$k\n";
+	$network = $k if $k =~ m/^$stub/;
+}
 
 my $build='';
 if ($hd->{$network}->{'docker_build'}) {
@@ -32,8 +37,9 @@ if ($hd->{$network}->{'docker_build'}) {
 	$build =~ m/.*_(.*)_.*/;
 	$build = $1;
 
-	print "$build\n";
 } else {
 	printf "Cannot find $network in Test Network inventory!\n";
 	exit 2
 }
+#warn "\nUsing:\t$network with $build\n";
+print "$build\n";
