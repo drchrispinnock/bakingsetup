c45c0c647a40a29d3cf5359c6e4d03696b3673c2
Chris Pinnock <chris_pinnock@mac.com>
2022-01-20 11:31:13 +0000

    fix

--- mondaynet/mk-warez.sh
+++ mondaynet/mk-warez.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 cd $HOME
 buildid=`cat branch.txt`
-tar zcvf tezos-$buildid.tar.gz tezos/tezos-* tezos/active_protocol_versions tezos/active_testing_protocol_versions
+tar zvf tezos-$buildid.tar.gz tezos/tezos-* tezos/active_protocol_versions tezos/active_testing_protocol_versions
