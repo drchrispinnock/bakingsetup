6ca6f28f0253bb7d35f003ba0e665eb94ccfa920
Chris Pinnock <chris_pinnock@mac.com>
2022-01-20 19:15:11 +0000

    only restart on monday & daily

--- mondaynet/mondaynet-setup.sh
+++ mondaynet/mondaynet-setup.sh
@@ -46,12 +46,14 @@ if [ "$?" != "0" ]; then
 	crontab - < /tmp/_cron
 fi
 
-crontab -l > /tmp/_cron
-grep mondaynet-setup.sh /tmp/_cron >/dev/null 2>&1
-if [ "$?" != "0" ]; then
-	echo "Adding start scripts to crontab"
-	echo "$freq         /bin/bash $me/mondaynet/mondaynet-setup.sh >$HOME/setup-log.txt 2>&1" >> /tmp/_cron
-	crontab - < /tmp/_cron
+if [ "$testnetwork" = "mondaynet" || "$testnetwork" = "dailynet" ]; then
+	crontab -l > /tmp/_cron
+	grep mondaynet-setup.sh /tmp/_cron >/dev/null 2>&1
+	if [ "$?" != "0" ]; then
+		echo "Adding start scripts to crontab"
+		echo "$freq         /bin/bash $me/mondaynet/mondaynet-setup.sh >$HOME/setup-log.txt 2>&1" >> /tmp/_cron
+		crontab - < /tmp/_cron
+	fi
 fi
 
 # Setup the network names for comparison
