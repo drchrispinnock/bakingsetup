46120f134a98eefd77b47f2e5b704d7d89608d7f
Chris Pinnock <chris_pinnock@mac.com>
2022-01-20 12:58:33 +0000

    refactor

--- kill.sh
+++ kill.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# This script has the Ming Vase license. If you use the script
-# and it breaks you get to keep the pieces.
+# Stop a baking setup. 
+# Chris Pinnock 2022
+# MIT license
+#
 
 # Defaults - override in the configuration file (shell)
 #
@@ -12,25 +14,24 @@ if [ -z "$1" ]; then
 	exit 1
 fi
 
+if [ `whoami` != $username ]; then
+        echo "Must be run by $username"
+        exit 3;
+fi
+
 source $1
-pidfilebase=$piddir/_pid_tezos_$name
 
 if [ -z "$name" ]; then
 	echo "I need name set"
 	exit 1
 fi
 
-if [ `whoami` != $username ]; then
-        echo "Must be run by $username"
-        exit 3;
-fi
-
+pidfilebase=$piddir/_pid_tezos_$name
 
 for pidfile in ${pidfilebase}_node* ${pidfilebase}_baker* \
 		${pidfilebase}_endorser* ${pidfilebase}_accuser*; do
 
-
-	if [ -f $pidfile ]; then
+if [ -f $pidfile ]; then
 		pid=`cat $pidfile`
 		kill -TERM $pid
 		rm $pidfile
