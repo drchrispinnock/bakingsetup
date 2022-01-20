5f6333fff5ee972bc201a660cb6d4bb33670ceff
Chris Pinnock <chris_pinnock@mac.com>
2022-01-20 13:10:43 +0000

    fix username logic

--- kill.sh
+++ kill.sh
@@ -8,18 +8,22 @@
 # Defaults - override in the configuration file (shell)
 #
 piddir=/tmp
+username=`whoami` # Usually overridden in config
 
 if [ -z "$1" ]; then
 	echo "Usage: $0 configfile"
 	exit 1
 fi
 
+source $1
+
+# Pedestrian
+#
 if [ `whoami` != $username ]; then
         echo "Must be run by $username"
         exit 3;
 fi
 
-source $1
 
 if [ -z "$name" ]; then
 	echo "I need name set"
