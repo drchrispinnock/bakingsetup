d48b7e810c082ae6cdb244fcf6fd7c50938f38b5
Chris Pinnock <chris_pinnock@mac.com>
2022-07-18 16:04:12 +0100

    include new active protocols

--- mondaynet/mondaynet-start.sh
+++ mondaynet/mondaynet-start.sh
@@ -183,7 +183,7 @@ if [ -f "$HOME/.build" ]; then
 	# Save the build for the next boot just in case
 	#
 	cd ..
-	tar zcf $HOME/tezos-$branch.tar.gz tezos/tezos-* tezos/active_protocol_versions tezos/active_testing_protocol_versions tezos/sc_rollup_protocol_versions tezos/tx_rollup_protocol_versions
+	tar zcf $HOME/tezos-$branch.tar.gz tezos/tezos-* tezos/active_protocol_versions tezos/active_testing_protocol_versions tezos/sc_rollup_protocol_versions tezos/tx_rollup_protocol_versions tezos/script-inputs/*
 
 	rm -f "$HOME/.build"
 fi
