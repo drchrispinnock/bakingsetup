8ce305a51aae527cf91e7043bcf83211fcfbf1a4
Chris Pinnock <chris_pinnock@mac.com>
2022-09-20 13:58:27 +0100

    increment rust

--- mondaynet/mondaynet-start.sh
+++ mondaynet/mondaynet-start.sh
@@ -148,7 +148,7 @@ if [ -f "$HOME/.build" ]; then
 	echo "===> Installing rust"
 	wget https://sh.rustup.rs/rustup-init.sh > $buildlogs/rust.txt 2>&1
 	chmod +x rustup-init.sh
-	./rustup-init.sh --profile minimal --default-toolchain 1.52.1 -y >> $buildlogs/rust.txt 2>&1
+	./rustup-init.sh --profile minimal --default-toolchain 1.60.0 -y >> $buildlogs/rust.txt 2>&1
 	source $HOME/.cargo/env
 	rm -f rustup-init.sh
 	rm -f rustup-init.sh.?
