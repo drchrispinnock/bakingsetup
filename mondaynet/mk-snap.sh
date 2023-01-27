d1a6be24f5c061c76b90de44800932ddfd43d13a
Chris Pinnock <chris_pinnock@mac.com>
2023-01-27 19:03:45 +0000

    html

--- mondaynet/mk-snap.sh
+++ mondaynet/mk-snap.sh
@@ -11,11 +11,13 @@
 mkdir -p $snapshotdir
 now=`date +%Y%m%d%H%M`
 
-$HOME/tezos/octez-node snapshot export $snapshotdir/$name-$now."snapshot" --block head --rolling
+$HOME/tezos/octez-node snapshot export $snapshotdir/$name-rolling-snapshot" --block head --rolling
 [ $? != "0" ] && echo "Snapshot failed!" && exit 2
 
-aws s3 cp $snapshotdir/$name-$now."snapshot" $s3bucket
-rm -f $name-$now."snapshot"
+echo "<html><a href="$name-rolling-snapshot">Mondaynet Snapshot created at $now</a></html>" > $snapshotdir/index.html
+
+aws s3 sync $snapshotdir/ $s3bucket/
+rm -f $snapshotdir/$name-rolling-snapshot
 
 
 
