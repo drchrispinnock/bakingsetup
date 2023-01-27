b06364d2da845f84ea7b84ef9f02c15462133e46
Chris Pinnock <chris_pinnock@mac.com>
2023-01-27 19:06:33 +0000

    fix

--- mondaynet/mk-snap.sh
+++ mondaynet/mk-snap.sh
@@ -11,7 +11,7 @@
 mkdir -p $snapshotdir
 now=`date +%Y%m%d%H%M`
 
-$HOME/tezos/octez-node snapshot export $snapshotdir/$name-rolling-snapshot" --block head --rolling
+$HOME/tezos/octez-node snapshot export $snapshotdir/$name-rolling-snapshot --block head --rolling
 [ $? != "0" ] && echo "Snapshot failed!" && exit 2
 
 echo "<html><a href=\"$name-rolling-snapshot\">Mondaynet Snapshot created at $now</a></html>" > $snapshotdir/index.html
